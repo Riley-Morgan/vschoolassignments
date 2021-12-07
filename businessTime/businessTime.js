3d7abc0761eba7e715bda67d6270cf307edc8549
Riley Morgan <morgaril0@gmail.com>
2021-12-06 19:09:40 -0800

    added first five assignments to github

---
+++ businessTime/businessTime.js
@@ -0,0 +1,9 @@
+var button1 = document.getElementsById("fake1")
+button1.addEventListener("click", function() {
+    alert("Coming soon!")
+})
+
+var button2 = document.getElementById("fake2")
+button2.addEventListener("click", function() {
+    alert("Coming soon!")
+})
